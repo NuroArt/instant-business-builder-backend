48e875858934f1fd23e496c22018c295573c8848
Jonathan Scott <jonathans8419@gmail.com>
2026-09-03 17:20:50 -0500

    Delete brandingpack.js

--- brandingpack.js
+++
@@ -1,9 +0,0 @@
-// handlers/brandingpack.js
-
-const { sendOfferDetail } = require("./offerDetail");
-
-async function handleBrandingPack(chatId) {
-  await sendOfferDetail(chatId, "branding");
-}
-
-module.exports = handleBrandingPack;
