da4fcaeca8daaebb2356be37e64e9973949be8eb
Jonathan Scott <jonathans8419@gmail.com>
2026-09-03 17:20:10 -0500

    Delete automationpack.js

--- automationpack.js
+++
@@ -1,9 +0,0 @@
-// handlers/automationpack.js
-
-const { sendOfferDetail } = require("./offerDetail");
-
-async function handleAutomationPack(chatId) {
-  await sendOfferDetail(chatId, "automation");
-}
-
-module.exports = handleAutomationPack;
