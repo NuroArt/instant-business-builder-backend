732ab060df2d46b4963cc9930230da0dc3964190
Jonathan Scott <jonathans8419@gmail.com>
2026-09-03 17:24:56 -0500

    Delete websitepack.js

--- websitepack.js
+++
@@ -1,9 +0,0 @@
-// handlers/websitepack.js
-
-const { sendOfferDetail } = require("./offerDetail");
-
-async function handleWebsitePack(chatId) {
-  await sendOfferDetail(chatId, "website");
-}
-
-module.exports = handleWebsitePack;
